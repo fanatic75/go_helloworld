c5297837251dbfa2a689a3df46c6fe802f92e3d5
Prateek Banga <prateekbanga12@gmail.com>
2019-08-04 17:13:58 +0530

    added the code to repository, server is not showing the log every 5 second but the client is hitting the server every 5 secondthat's for sure.

---
+++ client/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "hello"
+)
+
+func schedule(what func(), delay time.Duration) chan bool {
+	stop := make(chan bool)
+
+	go func() {
+		for {
+			what()
+			select {
+			case <-time.After(delay):
+			case <-stop:
+				return
+			}
+		}
+	}()
+
+	return stop
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	ping := func() {
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Fatalf("err : %v", err)
+		}
+		log.Printf("just hit the server: %v", r.Message)
+	}
+
+	stop := schedule(ping, 5*time.Second)
+	time.Sleep(200 * time.Second)
+	stop <- true
+	time.Sleep(5 * time.Second)
+
+}
